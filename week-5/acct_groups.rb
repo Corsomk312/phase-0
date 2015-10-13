073d94ab8ecdbc384d84bc57f223a767722a64c6
Corsomk312 <corsomk312@gmail.com>
2015-10-13 13:08:12 -0400

    complete 5.6

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -4,11 +4,11 @@ def acct_grps(array)
 	j=1
 	array.each do |i|
 		if j <= (array.length/4)
-			array_hash[i] = j
+			array_hash[i] = "Accountability Group: #{j}"
 			j += 1 
 		else 
 			j = 1
-			array_hash[i] = j
+			array_hash[i] = "Accountability Group: #{j}"
 			j += 1
 		end
 	end
@@ -36,6 +36,22 @@ end
 	# 	end
 	# end
 	# puts accountability_group_3
+
+
+# What was the most interesting and most difficult part of this challenge?
+# I feel like the most difficult part for me was realizing to use a hash. I seem to think you can do the same things with arrays that you can with hashes, but i'm starting to see that you can really use a hash for a lot more things that you can an array.
+
+# Do you feel you are improving in your ability to write pseudocode and break the problem down?
+# Yes I do. psuedocode allows me to take a look at the big picture and come up with a solution without worrying about code. It lets me solve the problem and then go back and see. I even had to re-psuedo code this one. I do it physically writing down notes, just helps me to keep things straighter. 
+
+# Was your approach for automating this task a good solution? What could have made it even better?
+# I think this was a pretty simple solution. It doesn't have much fat on it, just an if/else statement within a .each method. I do foresee problems with the code if you were to remove someone in the middle of the program, it would then push everyone down one. But for setting up accountability groups, nobody will have less than 4 and only a max of 3 groups will have 5.
+
+# What data structure did you decide to store the accountability groups in and why?
+# I used a hash because this allowed me to use the key to hold everyone's unique name, while using the value to place them into an accountability group up to however many I'd like. Plus since we only needed to store 2 pieces of information (name and acct. group) a hash seemed perfectly built to handle this.
+
+# What did you learn in the process of refactoring your initial solution? Did you learn any new Ruby methods?
+	#I used a couple methods in my intial solution and I couldn't find anything that would help shorten my code in the docs. 
 
 
 
