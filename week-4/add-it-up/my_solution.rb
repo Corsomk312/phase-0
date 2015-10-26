caf7fb89b50d91f02e4d236db87101c665e09451
Corsomk312 <corsomk312@gmail.com>
2015-10-26 11:36:07 -0400

    4.7 complete

--- week-4/add-it-up/my_solution.rb
+++ week-4/add-it-up/my_solution.rb
@@ -11,12 +11,22 @@
 # 0. total Pseudocode
 # make sure all pseudocode is commented out!
 
-# Input: 
-# Output: 
+# Input: Input is a series of numbers
+# Output: Sum of all the numbers
 # Steps to solve the problem.
-
+	#Iterate through the array and add each number together until you are through the entire array.
 
 # 1. total initial solution
+
+def total(nums)
+	total = 0
+
+	nums.each do |i|
+		total += i
+	end
+	return total
+end
+
 
 
 
@@ -26,21 +36,33 @@
 
 # 4. sentence_maker pseudocode
 # make sure all pseudocode is commented out!
-# Input:
-# Output:
+# Input:array of individual strings
+# Output: One grammatically correct string
 # Steps to solve the problem.
+	# iterate through the array. Capitalize the first word. Add each word to the string with a space in between, and end the sentence with a period.
 
 
 # 5. sentence_maker initial solution
 def sentence_maker(words)
-	i=0
 	sentence = ''
+	i = 0
 
 	while i < words.length
-		if words.length == 0
-			sentence = sentence + words[0].capitalize
-			
+		if i == 0 
+			sentence = sentence + words[i].to_s.capitalize + ' '
+			i += 1
+		elsif i == (words.length - 1)
+			sentence = sentence + words[i].to_s + '.'
+			i += 1
+		else
+			sentence = sentence + words[i].to_s + ' '
+			i += 1
+		end
 	end
+	p sentence
+end 
+
+sentence_maker(["alaska", "has", "over", 586, "thousand", "miles"])
 
 
 # 6. sentence_maker refactored solution
