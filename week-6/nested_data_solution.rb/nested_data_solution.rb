425d9bcb48677ff515bc6afca7314d0471861933
Corsomk312 <corsomk312@gmail.com>
2015-11-06 14:34:58 -0600

    Complete 6.5

--- week-6/nested_data_solution.rb/nested_data_solution.rb
+++ week-6/nested_data_solution.rb/nested_data_solution.rb
@@ -72,3 +72,17 @@ end
 
 recursive_method(startup_names)
 p startup_names
+
+# #Reflection
+# What are some general rules you can apply to nested arrays?
+
+# You always want to make sure that you are keeping track of where you in the nest. It is very easy to get confused, and you want to make sure that you are in the place you think you are. 
+
+# What are some ways you can iterate over nested arrays?
+
+# You can do a test to see if the element is another array/hash before trying to modify the element. 
+
+
+# Did you find any good new methods to implement or did you re-use one you were already familiar with? What was it and why did you decide that was a good option?
+
+# We used map! and kind_of?, which I had used both of them before. I decided it was a good option because it allowed us to be destructive and edit the original array. It was definitely good to get more practice with them and to see them being useful in a different way.
